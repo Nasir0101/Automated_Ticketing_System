d79047018d3d4cdafdad0284f615b4686da4fc98
nasirali99 <nasir.ali@venturedive.com>
2022-01-10 00:38:43 +0500

    Authorization Filter Added

--- src/main/java/com/example/automatedticketingsystem/common/util/JwtUtil.java
+++ src/main/java/com/example/automatedticketingsystem/common/util/JwtUtil.java
@@ -3,7 +3,6 @@ package com.example.automatedticketingsystem.common.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -17,24 +16,24 @@ import static com.example.automatedticketingsystem.common.constant.SecurityConst
 @Service
 public class JwtUtil {
 
-    public String extractUsername(String token) {
+    public static String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    public Date extractExpiration(String token) {
+    public static Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    public static <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
-    public Claims extractAllClaims(String token) {
+    public static Claims extractAllClaims(String token) {
         return Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
     }
 
-    private Boolean isTokenExpired(String token) {
+    public static Boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
@@ -48,12 +47,5 @@ public class JwtUtil {
         return TOKEN_PREFIX + Jwts.builder().setClaims(claims).setSubject(userName).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(SignatureAlgorithm.HS256, SECRET).compact();
-    }
-
-    public Boolean validateToken(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        Claims claims = extractAllClaims(token);
-        String role = userDetails.getAuthorities().toArray()[0].toString();
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token) && claims.get("role").equals(role));
     }
 }
--- src/main/java/com/example/automatedticketingsystem/filter/AuthorizationFilter.java
+++ src/main/java/com/example/automatedticketingsystem/filter/AuthorizationFilter.java
@@ -1,11 +1,58 @@
 package com.example.automatedticketingsystem.filter;
 
+import com.example.automatedticketingsystem.common.constant.SecurityConstants;
+import com.example.automatedticketingsystem.common.util.JwtUtil;
+import com.example.automatedticketingsystem.service.Implementations.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class AuthorizationFilter extends BasicAuthenticationFilter {
 
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
     public AuthorizationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        String header = req.getHeader(SecurityConstants.HEADER_STRING);
+
+        if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(req, res);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(SecurityConstants.HEADER_STRING);
+        if (token != null) {
+            token = token.replace(SecurityConstants.TOKEN_PREFIX, "");
+            String userName = JwtUtil.extractUsername(token);
+            Boolean isExpired = JwtUtil.isTokenExpired(token);
+
+            if (userName != null && !isExpired) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+                if (userDetails == null) return null;
+                return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            }
+        }
+        return null;
     }
 }
